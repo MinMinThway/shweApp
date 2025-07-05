1578432d9b81f94956cf7237234d566bfd0f70b0
Min Thway Khaing <110376802+MinThwayKhaing@users.noreply.github.com>
2025-07-06 00:44:01 +0630

    MTK

--- src/api/exchangeApi.js
+++ src/api/exchangeApi.js
@@ -1,24 +1,36 @@
 // src/api/exchangeRateApi.js
-import axios from "axios";
+import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const fetchExchangeRates = async (
-  startDate,
-  endDate,
-  page = 0,
-  size = 10
-) => {
-  const token = localStorage.getItem("accessToken");
+export const fetchExchangeRates = async (startDate, endDate, page = 0, size = 10) => {
+  const token = localStorage.getItem('accessToken');
 
   const response = await axios.get(
     `${API_BASE_URL}api/v1/exchange-rates/filter?startDate=${startDate}&endDate=${endDate}&page=${page}&size=${size}`,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     }
   );
+
+  return response.data;
+};
+
+/**
+ * Create a new exchange rate
+ * @param {Object} payload - Exchange rate object: { date, time, buyMmk, sellMmk }
+ */
+export const createExchangeRate = async (payload) => {
+  const token = localStorage.getItem('accessToken');
+
+  const response = await axios.post(`${API_BASE_URL}api/v1/exchange-rates`, payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  });
 
   return response.data;
 };
